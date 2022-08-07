900e64f5075b13fc43b5e6ac45eceb1cf4b94e25
dcchivian <dcchivian@lbl.gov>
2022-08-07 07:01:43 +0000

    debugging label pos

--- lib/kb_genetree/kb_genetreeImpl.py
+++ lib/kb_genetree/kb_genetreeImpl.py
@@ -3824,7 +3824,7 @@ class kb_genetree:
                 label_start_pos = feature_element_start_pos
                 label_y_pos = y_pos_base
                 print ("ROW: {} y_pos: {}".format(row_n, label_y_pos))
-                label_y_pos += 0.1 * float(row_n) / float(max_row_n)
+                label_y_pos += 0.8 * float(row_n) / float(max_row_n)
                 vert_align = "center"   # top,bottom,center,baseline,center_baseline
                 
 #                if Global_State['genomebrowser_window_bp_width'] > text_disp_window_bp_limit or this_label_show_top:
