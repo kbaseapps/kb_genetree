b7f53d325439882451943e28d0ab8dfb874b9a62
dcchivian <dcchivian@lbl.gov>
2022-08-07 03:52:02 +0000

    playing with tree height

--- lib/kb_genetree/kb_genetreeImpl.py
+++ lib/kb_genetree/kb_genetreeImpl.py
@@ -4285,7 +4285,9 @@ class kb_genetree:
                 img_units = "in"
                 img_pix_width = 200
                 img_in_width = round(float(img_pix_width)/float(dpi), 1)
-                img_in_height = 40 * total_rows
+                #img_pix_height = 40 * total_rows
+                img_pix_height = 10 * total_rows
+                img_in_height = round(float(img_pix_height)/float(dpi), 1)
                 self.log(console,"SAVING TREE IMAGE")  # DEBUG
                 treeObj.render(tree_img_path, w=img_in_width, h=img_in_height, units=img_units, dpi=dpi, tree_style=ts)
                 #treeObj.render(tree_img_path, w=img_in_width, units=img_units, dpi=dpi, tree_style=ts)
